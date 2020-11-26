e24abde3b81d5954be6469551854e0946b5ff48c
Alexander Podkopaev <p-alex1990@mail.ru>
2020-11-26 16:07:23 +0500

    Добавлена логика возвращения с фрагмента фильма на список фильмов

--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/FragmentMoviesDetails.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/FragmentMoviesDetails.kt
@@ -1,18 +1,39 @@
 package com.alexanderpodkopaev.androidacademyproject
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class FragmentMoviesDetails : Fragment() {
+    var clickListenerCurrentMovie:ClickListenerCurrentMovie? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_movies_details,container,false)
-
+        val view = inflater.inflate(R.layout.fragment_movies_details,container,false)
+        view?.findViewById<TextView>(R.id.tvBack)?.apply { setOnClickListener { clickListenerCurrentMovie?.onClickBack() } }
+        view?.findViewById<ImageView>(R.id.ivBack)?.apply { setOnClickListener { clickListenerCurrentMovie?.onClickBack() } }
+        return view
     }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is ClickListenerCurrentMovie) {
+            clickListenerCurrentMovie = context
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        clickListenerCurrentMovie = null
+    }
+}
+interface ClickListenerCurrentMovie {
+    fun onClickBack()
 }
--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/MainActivity.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/MainActivity.kt
@@ -3,19 +3,31 @@ package com.alexanderpodkopaev.androidacademyproject
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), ClickListenerItemMovie {
+class MainActivity : AppCompatActivity(), ClickListenerItemMovie, ClickListenerCurrentMovie {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .add(R.id.flFragment, FragmentMoviesList())
-            .commit()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.flFragment, FragmentMoviesList())
+                .commit()
+        } else {
+            supportFragmentManager.findFragmentByTag(TAG_DETAILS) as? FragmentMoviesDetails
+        }
     }
 
     override fun onClickItemMovie() {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.flFragment, FragmentMoviesDetails())
+            .replace(R.id.flFragment, FragmentMoviesDetails(),TAG_DETAILS)
             .addToBackStack(null)
             .commit()
+    }
+
+    override fun onClickBack() {
+        supportFragmentManager.popBackStack()
+    }
+
+    companion object {
+        private const val TAG_DETAILS = "DETAILS"
     }
 }
