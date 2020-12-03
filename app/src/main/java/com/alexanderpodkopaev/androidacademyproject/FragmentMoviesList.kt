598b547531f971f42a6e2d8da2a5e116e13ce43c
Alexander Podkopaev <p-alex1990@mail.ru>
2020-12-03 18:26:13 +0500

    Добавлены разделитель

--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/FragmentMoviesList.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/FragmentMoviesList.kt
@@ -5,11 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.alexanderpodkopaev.androidacademyproject.adapter.MovieClickListener
 import com.alexanderpodkopaev.androidacademyproject.adapter.MoviesAdapter
-import com.alexanderpodkopaev.androidacademyproject.data.MovieModel
 import com.alexanderpodkopaev.androidacademyproject.data.getMovies
 
 class FragmentMoviesList : Fragment(), MovieClickListener {
@@ -27,8 +28,18 @@ class FragmentMoviesList : Fragment(), MovieClickListener {
         adapter?.bindMovies(generatedMovies)
         adapter?.onMovieClickListener = this
         recyclerViewMovies = view.findViewById(R.id.rvMoviesList)
-        recyclerViewMovies?.layoutManager = GridLayoutManager(context,calculateNoOfColumns(context,180f))
+        recyclerViewMovies?.layoutManager =
+            GridLayoutManager(context, calculateNoOfColumns(context, 180f))
         recyclerViewMovies?.adapter = adapter
+        recyclerViewMovies?.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                LinearLayoutManager.HORIZONTAL
+            ).apply {
+                context?.getDrawable(R.drawable.divider)?.let {
+                    setDrawable(it)
+                }
+            })
 
         return view
     }
@@ -37,7 +48,7 @@ class FragmentMoviesList : Fragment(), MovieClickListener {
     override fun onMovieClick(movieTitle: String) {
         val movieFragment = FragmentMoviesDetails()
         val bundle = Bundle().apply {
-            putString(TITLE,movieTitle)
+            putString(TITLE, movieTitle)
         }
         movieFragment.arguments = bundle
         fragmentManager?.beginTransaction()
