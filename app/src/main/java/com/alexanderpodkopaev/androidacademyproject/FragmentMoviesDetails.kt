23d97be1272e6c38b9671138c41e1a0e543be469
Alexander Podkopaev <p-alex1990@mail.ru>
2020-12-03 17:56:00 +0500

    В фрагмент добавлено заполнение view из полученного фильма. Добавлен разделитель для recyclerview

--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/FragmentMoviesDetails.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/FragmentMoviesDetails.kt
@@ -5,10 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.alexanderpodkopaev.androidacademyproject.adapter.ActorsAdapter
+import com.alexanderpodkopaev.androidacademyproject.data.MovieModel
 
 class FragmentMoviesDetails : Fragment() {
+    private var actorsAdapter: ActorsAdapter? = null
 
 
     override fun onCreateView(
@@ -21,7 +28,47 @@ class FragmentMoviesDetails : Fragment() {
             ?.apply { setOnClickListener { fragmentManager?.popBackStack() } }
         view?.findViewById<ImageView>(R.id.ivBack)
             ?.apply { setOnClickListener { fragmentManager?.popBackStack() } }
+        val movie = findMovie(arguments?.getString(FragmentMoviesList.TITLE))
+        actorsAdapter = ActorsAdapter()
+        actorsAdapter?.bindActors(movie.actors)
+        view.findViewById<RecyclerView>(R.id.rvActors).apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = actorsAdapter
+            addItemDecoration(DividerItemDecoration(context,LinearLayoutManager.HORIZONTAL).apply { context.getDrawable(R.drawable.divider)?.let {
+                setDrawable(it)
+            } })
+        }
+        view.findViewById<ImageView>(R.id.ivBackground).apply {
+            setImageDrawable(resources.getDrawable(movie.picture, context.theme))
+        }
+        view.findViewById<TextView>(R.id.tvTitle).apply {
+            text = movie.title
+        }
+        view.findViewById<TextView>(R.id.tvAge).apply {
+            text = getString(R.string.text_age, movie.age.toString())
+        }
+        view.findViewById<TextView>(R.id.tvGanre).apply {
+            text = movie.genre
+        }
+        view.findViewById<RatingBar>(R.id.rbStar).apply {
+            progress = movie.rating
+        }
+        view.findViewById<TextView>(R.id.tvReview).apply {
+            text = getString(R.string.text_review, movie.countReview.toString())
+        }
+        view.findViewById<TextView>(R.id.tvDescription).apply {
+            text = movie.storyline
+        }
+
         return view
+    }
+
+
+    private fun findMovie(movieTitle: String?): MovieModel {
+        FragmentMoviesList.generatedMovies.forEach {
+            if (it.title == movieTitle) return it
+        }
+        throw IllegalArgumentException()
     }
 
 }
