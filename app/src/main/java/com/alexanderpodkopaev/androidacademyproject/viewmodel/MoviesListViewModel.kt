5086faff688ab797121544c6c0911e5b98581903
Alexander Podkopaev <p-alex1990@mail.ru>
2020-12-24 10:38:49 +0500

    Добавлена проверка наличия списка фильмов в liveData, для исключения повторной загрузки

--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/viewmodel/MoviesListViewModel.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/viewmodel/MoviesListViewModel.kt
@@ -1,24 +1,23 @@
 package com.alexanderpodkopaev.androidacademyproject.viewmodel
 
-import android.app.Application
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.alexanderpodkopaev.androidacademyproject.data.Movie
-import com.alexanderpodkopaev.androidacademyproject.data.loadMovies
 import com.alexanderpodkopaev.androidacademyproject.repo.MoviesRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MoviesListViewModel(private val repository: MoviesRepository) : ViewModel() {
 
     private var _moviesList = MutableLiveData<List<Movie>>(emptyList())
-    var moviesList: LiveData<List<Movie>> = _moviesList
+    val moviesList: LiveData<List<Movie>> = _moviesList
 
     fun fetchMovies() {
         viewModelScope.launch {
-            val movies = repository.getMovies()
-            withContext(Dispatchers.Main) {
-                _moviesList.postValue(movies)
+            if (moviesList.value.isNullOrEmpty()) {
+                val movies = repository.getMovies()
+                _moviesList.value = movies
             }
         }
     }
