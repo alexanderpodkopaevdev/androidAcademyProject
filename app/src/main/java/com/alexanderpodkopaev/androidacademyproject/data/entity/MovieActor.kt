4eee8f7a82ceaf90f28b78eca76ee1263ec61657
Alexander Podkopaev <p-alex1990@mail.ru>
2021-01-27 10:00:49 +0500

    Добавил ключи, и каскадное удаление

--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/data/entity/MovieActor.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/data/entity/MovieActor.kt
@@ -1,8 +1,23 @@
 package com.alexanderpodkopaev.androidacademyproject.data.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 
-@Entity(primaryKeys = ["movieId", "actorId"])
+@Entity(
+    primaryKeys = ["movieId", "actorId"], foreignKeys = [
+        ForeignKey(
+            entity = MovieEntity::class,
+            childColumns = arrayOf("movieId"),
+            parentColumns = arrayOf("mId"),
+            onDelete = ForeignKey.CASCADE
+        ), ForeignKey(
+            entity = ActorEntity::class,
+            childColumns = arrayOf("actorId"),
+            parentColumns = arrayOf("aId")
+        )
+    ]
+)
+
 data class MovieActor(
     val movieId: Int,
     val actorId: Int
--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/data/entity/MovieGenre.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/data/entity/MovieGenre.kt
@@ -1,8 +1,22 @@
 package com.alexanderpodkopaev.androidacademyproject.data.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 
-@Entity(primaryKeys = ["movieId", "genreId"])
+@Entity(
+    primaryKeys = ["movieId", "genreId"], foreignKeys = [
+        ForeignKey(
+            entity = MovieEntity::class,
+            childColumns = arrayOf("movieId"),
+            parentColumns = arrayOf("mId"),
+            onDelete = ForeignKey.CASCADE
+        ), ForeignKey(
+            entity = GenreEntity::class,
+            childColumns = arrayOf("genreId"),
+            parentColumns = arrayOf("gId")
+        )
+    ]
+)
 data class MovieGenre(
     val movieId: Int,
     val genreId: Int
