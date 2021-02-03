2a6570ac3dc254ec2ca0a747ac9461ef7c92b5a6
Alexander Podkopaev <p-alex1990@mail.ru>
2021-02-03 17:19:19 +0500

    Сделал запуск единственной задачи

--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/service/WorkRepository.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/service/WorkRepository.kt
@@ -8,10 +8,14 @@ import java.util.concurrent.TimeUnit
 class WorkRepository {
     private val constraints = Constraints.Builder()
         .setRequiredNetworkType(NetworkType.UNMETERED)
-//        .setRequiresCharging(true)
+        .setRequiresCharging(true)
         .build()
     val constrainedRequest =
         PeriodicWorkRequest.Builder(MoviesUpdateWorker::class.java, 8, TimeUnit.SECONDS)
             .setConstraints(constraints)
             .build()
+
+    companion object {
+        const val WORK_TAG = "WORK_TAG"
+    }
 }
--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/ui/MainActivity.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.alexanderpodkopaev.androidacademyproject.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.WorkManager
 import com.alexanderpodkopaev.androidacademyproject.R
 import com.alexanderpodkopaev.androidacademyproject.service.WorkRepository
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        WorkManager.getInstance(applicationContext).enqueue(WorkRepository().constrainedRequest)
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(WorkRepository.WORK_TAG, ExistingPeriodicWorkPolicy.KEEP, WorkRepository().constrainedRequest)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .add(R.id.flFragment, FragmentMoviesList())
