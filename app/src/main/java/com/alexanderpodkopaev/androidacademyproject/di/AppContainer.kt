de263bc98dead4812c9233ff873f43349dc3dd72
Alexander Podkopaev <p-alex1990@mail.ru>
2021-02-04 14:14:33 +0500

    шедуллер перенесен в контейнер, запускается при старте application

--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/di/AppContainer.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/di/AppContainer.kt
@@ -1,13 +1,18 @@
 package com.alexanderpodkopaev.androidacademyproject.di
 
 import android.content.Context
+import androidx.work.WorkManager
 import com.alexanderpodkopaev.androidacademyproject.data.MoviesDatabase
 import com.alexanderpodkopaev.androidacademyproject.data.RetrofitModule
 import com.alexanderpodkopaev.androidacademyproject.repo.*
+import com.alexanderpodkopaev.androidacademyproject.service.MoviesSyncSchedule
 
 class AppContainer(context: Context) {
+
     private val database: MoviesDatabase = MoviesDatabase.create(context)
     private var dbDataSource: DBDataSource = DBDataSource(database)
+    private val workManager: WorkManager = WorkManager.getInstance(context)
     val moviesRepository: MoviesRepository = MoviesRepoImpl(RetrofitModule.moviesApi, dbDataSource)
     val actorsRepository: ActorsRepository = ActorsRepoImpl(RetrofitModule.moviesApi, dbDataSource)
+    val moviesSyncSchedule: MoviesSyncSchedule = MoviesSyncSchedule(workManager)
 }
--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/ui/MainActivity.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/ui/MainActivity.kt
@@ -2,17 +2,13 @@ package com.alexanderpodkopaev.androidacademyproject.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.WorkManager
 import com.alexanderpodkopaev.androidacademyproject.R
-import com.alexanderpodkopaev.androidacademyproject.service.WorkRepository
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(WorkRepository.WORK_TAG, ExistingPeriodicWorkPolicy.KEEP, WorkRepository().constrainedRequest)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .add(R.id.flFragment, FragmentMoviesList())
