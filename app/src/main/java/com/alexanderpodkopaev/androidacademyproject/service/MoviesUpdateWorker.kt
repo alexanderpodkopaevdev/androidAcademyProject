856bbf6c022d6546e0374439836b257620e05be1
Alexander Podkopaev <p-alex1990@mail.ru>
2021-02-12 14:47:38 +0500

    Добавлено появление оповещения и переход на карточку фильма

--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/service/MoviesUpdateWorker.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/service/MoviesUpdateWorker.kt
@@ -1,13 +1,20 @@
 package com.alexanderpodkopaev.androidacademyproject.service
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.net.toUri
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.alexanderpodkopaev.androidacademyproject.MyApp
+import com.alexanderpodkopaev.androidacademyproject.R
 import com.alexanderpodkopaev.androidacademyproject.data.model.Movie
 
-class MoviesUpdateWorker(context: Context, workerParameters: WorkerParameters) :
+class MoviesUpdateWorker(val context: Context, workerParameters: WorkerParameters) :
     CoroutineWorker(context, workerParameters) {
 
     override suspend fun doWork(): Result {
@@ -15,7 +22,10 @@ class MoviesUpdateWorker(context: Context, workerParameters: WorkerParameters) :
         Log.d("MyWorker", "Start worker")
         val moviesFromDB = container.moviesRepository.getMovies()
         val moviesFromNetwork = container.moviesRepository.getMovies(true)
-        if (moviesFromDB.isNotEmpty() && moviesFromNetwork.isNotEmpty()) findNewMostPopularAndSendNotification(moviesFromDB, moviesFromNetwork)
+        if (moviesFromDB.isNotEmpty() && moviesFromNetwork.isNotEmpty()) findNewMostPopularAndSendNotification(
+            moviesFromDB,
+            moviesFromNetwork
+        )
         return if (moviesFromNetwork.isNotEmpty()) {
             Result.success()
         } else {
@@ -23,12 +33,15 @@ class MoviesUpdateWorker(context: Context, workerParameters: WorkerParameters) :
         }
     }
 
-    private fun findNewMostPopularAndSendNotification(moviesFromDB: List<Movie>, moviesFromNetwork: List<Movie>) {
+    private fun findNewMostPopularAndSendNotification(
+        moviesFromDB: List<Movie>,
+        moviesFromNetwork: List<Movie>
+    ) {
         val diff = findNewFilms(moviesFromDB, moviesFromNetwork)
         if (diff.isNotEmpty()) {
             val mostPopularMovie = findMostPopular(diff)
             if (mostPopularMovie != null) {
-                sendNotification(mostPopularMovie)
+                showNotification(mostPopularMovie)
             }
         }
     }
@@ -44,8 +57,54 @@ class MoviesUpdateWorker(context: Context, workerParameters: WorkerParameters) :
         return diff.maxByOrNull { it.ratings }
     }
 
-    private fun sendNotification(mostPopularMovie: Movie) {
-        TODO("Not yet implemented")
+    private fun showNotification(mostPopularMovie: Movie) {
+        initialize()
+        Log.d("MyWorker", "Start notification")
+        val uri =
+            "com.alexanderpodkopaev.androidacademyproject://movies/movie/${mostPopularMovie.id}".toUri()
+        Log.d("MyWorker uri", uri.toString())
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            REQUEST_CONTENT,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val notify = NotificationCompat.Builder(context, CHANNEL_NEW_MESSAGES)
+            .setContentTitle(mostPopularMovie.title)
+            .setContentText(mostPopularMovie.overview)
+            .setSmallIcon(R.drawable.star_icon_disable)
+            .setPriority(NotificationManagerCompat.IMPORTANCE_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+        notificationManagerCompat.notify(MOVIE_TAG, mostPopularMovie.id, notify)
+    }
+
+    private val notificationManagerCompat: NotificationManagerCompat =
+        NotificationManagerCompat.from(context)
+
+    fun initialize() {
+        if (notificationManagerCompat.getNotificationChannel(CHANNEL_NEW_MESSAGES) == null) {
+
+            val channel = NotificationChannelCompat.Builder(
+                CHANNEL_NEW_MESSAGES,
+                NotificationManagerCompat.IMPORTANCE_HIGH
+            )
+                .setName("New movie")
+                .setDescription("New movie")
+                .build()
+            notificationManagerCompat.createNotificationChannel(channel)
+
+
+        }
+    }
+
+    companion object {
+        private const val REQUEST_CONTENT = 1
+        private const val CHANNEL_NEW_MESSAGES = "new_movie"
+        private const val MOVIE_TAG = "movie"
+
     }
 
 }
--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/ui/MainActivity.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.alexanderpodkopaev.androidacademyproject.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.alexanderpodkopaev.androidacademyproject.R
 
@@ -13,7 +15,40 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .add(R.id.flFragment, FragmentMoviesList())
                 .commit()
+            if (intent != null) {
+                handleIntent(intent)
+            }
         }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Log.d("MyWorker", "Start notification")
+        if (intent != null) {
+            handleIntent(intent)
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        Log.d("MyWorker", "in handle")
+        when (intent.action) {
+            Intent.ACTION_VIEW -> {
+                Log.d("MyWorker", "in intent")
+
+                val id = intent.data?.lastPathSegment?.toIntOrNull()
+                if (id != null) {
+                    openMovie(id)
+                }
+            }
+        }
+    }
+
+    private fun openMovie(id: Int) {
+        Log.d("MyWorker", "open fragment")
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.flFragment, FragmentMoviesDetails.newInstance(id))
+            .addToBackStack(null)
+            .commit()
     }
 
 }
