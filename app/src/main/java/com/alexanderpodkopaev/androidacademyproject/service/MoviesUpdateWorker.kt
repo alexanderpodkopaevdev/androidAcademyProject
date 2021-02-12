438df0100f38e174bb2e65a2ed1935a8c9e627b0
Alexander Podkopaev <p-alex1990@mail.ru>
2021-02-12 09:29:40 +0500

    Добавлена логика поиска нового популярного фильмы

--- app/src/main/java/com/alexanderpodkopaev/androidacademyproject/service/MoviesUpdateWorker.kt
+++ app/src/main/java/com/alexanderpodkopaev/androidacademyproject/service/MoviesUpdateWorker.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.alexanderpodkopaev.androidacademyproject.MyApp
+import com.alexanderpodkopaev.androidacademyproject.data.model.Movie
 
 class MoviesUpdateWorker(context: Context, workerParameters: WorkerParameters) :
     CoroutineWorker(context, workerParameters) {
@@ -12,10 +13,39 @@ class MoviesUpdateWorker(context: Context, workerParameters: WorkerParameters) :
     override suspend fun doWork(): Result {
         val container = MyApp.container
         Log.d("MyWorker", "Start worker")
-        return if (container.moviesRepository.getMovies(true).isNotEmpty()) {
+        val moviesFromDB = container.moviesRepository.getMovies()
+        val moviesFromNetwork = container.moviesRepository.getMovies(true)
+        if (moviesFromDB.isNotEmpty() && moviesFromNetwork.isNotEmpty()) findNewMostPopularAndSendNotification(moviesFromDB, moviesFromNetwork)
+        return if (moviesFromNetwork.isNotEmpty()) {
             Result.success()
         } else {
             Result.failure()
         }
     }
+
+    private fun findNewMostPopularAndSendNotification(moviesFromDB: List<Movie>, moviesFromNetwork: List<Movie>) {
+        val diff = findNewFilms(moviesFromDB, moviesFromNetwork)
+        if (diff.isNotEmpty()) {
+            val mostPopularMovie = findMostPopular(diff)
+            if (mostPopularMovie != null) {
+                sendNotification(mostPopularMovie)
+            }
+        }
+    }
+
+    private fun findNewFilms(
+        moviesFromDB: List<Movie>,
+        moviesFromNetwork: List<Movie>
+    ): List<Movie> {
+        return moviesFromNetwork.toSet().minus(moviesFromDB.toSet()).toList()
+    }
+
+    private fun findMostPopular(diff: List<Movie>): Movie? {
+        return diff.maxByOrNull { it.ratings }
+    }
+
+    private fun sendNotification(mostPopularMovie: Movie) {
+        TODO("Not yet implemented")
+    }
+
 }
